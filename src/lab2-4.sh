9ad5c0154a153c3f2d128000a7c72c65c468d5a2
kchsugo <35268047+kchsugo@users.noreply.github.com>
2021-12-01 00:03:55 +0900

    Update lab2-4.sh

--- src/lab2-4.sh
+++ src/lab2-4.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
+echo "리눅스가 재미있나요?(yes/no)"
+read answer
+case $answer in
+    yes|y|Y|Yes|YES)
+    echo "다행입니다."
+    echo "더욱 열심히하세요^^";;
+    [nN]*)
+    echo "안타깝니요.";;
+    *)
+    echo "yes 아니면 no만 입력했어야죠"
+    exit 1;;
+esac
 
