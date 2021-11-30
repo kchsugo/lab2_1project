b321dadc270f71e90feea51178b7241ece494879
kchsugo <35268047+kchsugo@users.noreply.github.com>
2021-12-01 00:06:39 +0900

    Update lab2-6.sh

--- src/lab2-6.sh
+++ src/lab2-6.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+mkdir $1
+eval "cd files"
+for i in {1..5}
+do
+    touch files$i.txt
+done
+mkdir $1
+eval "cd files"
+for i in {1..5}
+do
+    touch files$1.txt
+done
+touch files.tar
